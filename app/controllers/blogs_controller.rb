b64c4bd5470ca23f0d9ac14919c5c3265b176384
Shawydu <dxydyl2@gmail.com>
2016-01-23 16:50:42 -0500

    quiz#2 update, add article model and simple form

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,2 +1,19 @@
 class BlogsController < ApplicationController
+	def index
+		@articles = Article.all
+	end
+
+	def new
+		@article = Article.new
+	end
+
+	def create
+		Article.create(article_params)
+		redirect_to root_path
+	end
+
+	private
+	def article_params
+		params.require(:article).permit(:title, :content)
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root 'blogs#index'
+
+  resources :blogs
 end
